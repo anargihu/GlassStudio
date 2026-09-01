f78e26bd58d362c30d00298b37dc82b6bd881ae9
anargihu <ndjd86d5fw@privaterelay.appleid.com>
2026-09-01 21:55:38 +0800

    Update GlassStudioApp.swift

--- GlassStudio/GlassStudioApp.swift
+++ GlassStudio/GlassStudioApp.swift
@@ -6,17 +6,17 @@ struct GlassStudioApp: App {
     var body: some Scene {
         WindowGroup {
             WebView()
+                .ignoresSafeArea()
         }
     }
 }
 
 struct WebView: UIViewRepresentable {
     func makeUIView(context: Context) -> WKWebView {
-        let webView = WKWebView()
+        let webView = WKWebView(frame: .zero)
 
-        if let url = Bundle.main.url(forResource: "index", withExtension: "html", subdirectory: "Website") {
-            let accessURL = url.deletingLastPathComponent()
-            webView.loadFileURL(url, allowingReadAccessTo: accessURL)
+        if let url = Bundle.main.url(forResource: "index", withExtension: "html") {
+            webView.loadFileURL(url, allowingReadAccessTo: Bundle.main.resourceURL!)
         }
 
         return webView

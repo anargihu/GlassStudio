96543a414a21bb2b37e7b2426539d480ea11f5ed
anargihu <ndjd86d5fw@privaterelay.appleid.com>
2026-09-03 12:57:45 +0800

    Update GlassStudioApp.swift

--- GlassStudio/GlassStudioApp.swift
+++ GlassStudio/GlassStudioApp.swift
@@ -15,21 +15,14 @@ struct WebView: UIViewRepresentable {
     func makeUIView(context: Context) -> WKWebView {
         let webView = WKWebView(frame: .zero)
 
-        if let url = Bundle.main.url(
-            forResource: "index",
-            withExtension: "html",
-            subdirectory: "Website"
-        ) {
-            webView.loadFileURL(
-                url,
-                allowingReadAccessTo: Bundle.main.bundleURL
-            )
-        } else {
-            webView.loadHTMLString(
-                "<h1>Website/index.html not found</h1>",
-                baseURL: nil
-            )
-        }
+        let websiteURL = Bundle.main.bundleURL
+            .appendingPathComponent("Website")
+            .appendingPathComponent("index.html")
+
+        webView.loadFileURL(
+            websiteURL,
+            allowingReadAccessTo: Bundle.main.bundleURL
+        )
 
         return webView
     }
